2ac266ecbc08bc268ff4d476209f7bbbd291bbee
vidhyaa12 <vidhyaa@Vidhyaas-MacBook-Air.local>
2015-10-19 23:56:20 -0700

    Longest continuous substring made of a single character

---
+++ answers/src/main/java/com/coding/solutions/basic/LongestContinuousSubstringsUsingSpecificChar.java
@@ -0,0 +1,54 @@
+package com.coding.solutions.basic;
+
+
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.*;
+
+public class LongestContinuousSubstringsUsingSpecificChar {
+    public static Set<String> findLongestSingleCharSeq(String str) {
+        if (StringUtils.isBlank(str)) {
+            return Collections.emptySet();
+        }
+
+        int maxLength = 0;
+        Set<String> singleCharSubstrings = new LinkedHashSet<String>();
+
+        for (int i = 0; i < str.length(); i++) {
+            StringBuilder stringBuilder = new StringBuilder();
+            int currSubstringLength = 1;
+            stringBuilder.append(str.charAt(i));
+
+            for (int j = i + 1; j < str.length(); j++) {
+                if (str.charAt(i) == str.charAt(j)) {
+                    currSubstringLength++;
+                    stringBuilder.append(str.charAt(j));
+                } else {
+                    break;
+                }
+            }
+
+            if (currSubstringLength >= maxLength) {
+                maxLength = currSubstringLength;
+                singleCharSubstrings.add(stringBuilder.toString());
+            }
+        }
+
+        Set<String> result = new LinkedHashSet<String>();
+
+        for (String s : singleCharSubstrings) {
+            if (s.length() == maxLength) {
+                result.add(s);
+            }
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(findLongestSingleCharSeq("aabbcb").toArray()));
+        System.out.println(Arrays.toString(findLongestSingleCharSeq("abc").toArray()));
+        System.out.println(Arrays.toString(findLongestSingleCharSeq("abab").toArray()));
+        System.out.println(Arrays.toString(findLongestSingleCharSeq("abccccaa").toArray()));
+    }
+}
