9812bedd29782a7c520e78ed4223b41a7d1fc662
vidhyaa12 <vidhyaa@Vidhyaas-MacBook-Air.local>
2015-12-12 16:52:21 -0800

    Fix bugs - 1

---
+++ answers/src/main/java/com/coding/solutions/heap/BuildHeap.java
@@ -0,0 +1,12 @@
+package com.coding.solutions.heap;
+
+import java.util.List;
+
+/**
+ * Build a heap in linear time.
+ */
+public class BuildHeap<T extends Comparable<T>> {
+    public BinaryHeap build(List<T> values) {
+
+    }
+}
